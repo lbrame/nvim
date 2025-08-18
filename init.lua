22d6b89e2996fc9e68f73023f0b55a754625ded2
Luca <45152996+lbrame@users.noreply.github.com>
2025-08-18 18:02:14 +0200

    Switch theme to Catppuccin

--- init.lua
+++ init.lua
@@ -905,7 +905,16 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 

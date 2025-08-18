c4ce3c90e6df41ffd2c2b658066109c1574eec8e
Luca <45152996+lbrame@users.noreply.github.com>
2025-08-18 17:52:06 +0200

    Add oil.nvim

--- init.lua
+++ init.lua
@@ -283,6 +283,17 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --

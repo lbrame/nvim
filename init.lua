0c412501c35a997f8d35218cbb05192d6359b958
Luca <45152996+lbrame@users.noreply.github.com>
2025-08-20 18:15:05 +0200

    Add alpha.nvim to add a pretty dashboard

--- init.lua
+++ init.lua
@@ -994,6 +994,13 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  -- Pretty Neovim dashboard
+  {
+    'goolord/alpha-nvim',
+    config = function()
+      require('alpha').setup(require('alpha.themes.dashboard').config)
+    end,
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and

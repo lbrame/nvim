392f88f910bb999552a25295113b5ad8dd36322b
Luca <45152996+lbrame@users.noreply.github.com>
2025-08-21 18:43:40 +0200

    Define custom aliases to cd into currently opened file

--- init.lua
+++ init.lua
@@ -214,6 +214,14 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- [[ Aliases ]]
+-- cd to directory current file is in globally
+vim.api.nvim_create_user_command('Cd', 'cd %:h', {})
+-- cd to directory current file is in within local buffer
+vim.api.nvim_create_user_command('Lcd', 'lcd %:h', {})
+-- cd to directory current file is in within local tab
+vim.api.nvim_create_user_command('Tcd', 'tcd %:h', {})
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 

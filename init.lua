44bcd468d831f7e22030764c694e4f6f0c825036
Luca <45152996+lbrame@users.noreply.github.com>
2025-09-01 19:58:53 +0200

    Invert ; and :

--- init.lua
+++ init.lua
@@ -214,6 +214,10 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Remap ; to : and then vice versa
+vim.keymap.set('n', ';', ':', { noremap = true })
+vim.keymap.set('n', ':', ';', { noremap = true })
+
 -- [[ User commands ]]
 -- cd to directory current file is in globally
 vim.api.nvim_create_user_command('Cd', 'cd %:h', {})

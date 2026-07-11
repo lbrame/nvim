62cd1b9a950d049dbe861b06eea37bd971cd7b1f
Luca <45152996+lbrame@users.noreply.github.com>
2026-07-11 14:24:48 +0200

    Behaviour: oil.lua: Remember last filesystem position

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -18,7 +18,31 @@ local tabl = {
   lazy = false,
 }
 
--- Set keymap to open Oil.nvim
-vim.keymap.set('n', '<C-n>', '<CMD>e .<CR>', { desc = 'Open oil.nvim' })
+--- Remember the last oil buffer URL within the current session.
+--- Restored when reopening oil via <C-n>.
+vim.g.last_oil_dir = nil
+
+local augroup = vim.api.nvim_create_augroup('OilLastDir', { clear = true })
+
+-- Save the oil buffer's URL when leaving it
+vim.api.nvim_create_autocmd('BufLeave', {
+  group = augroup,
+  pattern = 'oil://*',
+  callback = function()
+    local bufname = vim.api.nvim_buf_get_name(0)
+    if bufname and bufname ~= '' then
+      vim.g.last_oil_dir = bufname
+    end
+  end,
+})
+
+-- Open oil at the last-saved directory, falling back to current dir
+vim.keymap.set('n', '<C-n>', function()
+  if vim.g.last_oil_dir then
+    vim.cmd('edit ' .. vim.g.last_oil_dir)
+  else
+    vim.cmd 'edit .'
+  end
+end, { desc = 'Open oil.nvim (restores last position)' })
 
 return tabl

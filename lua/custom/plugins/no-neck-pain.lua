31efd35a8cbb6d5d43466a8e035b48e18a1ea5cf
Luca <45152996+lbrame@users.noreply.github.com>
2026-07-11 14:45:02 +0200

    Modify: run no-neck-pain at neovim startup

---
+++ lua/custom/plugins/no-neck-pain.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'shortcuts/no-neck-pain.nvim',
+    version = '*',
+    opts = {
+      autocmds = {
+        -- Automatically enables no-neck-pain on startup
+        enableOnVimEnter = true,
+      },
+    },
+  },
+}
